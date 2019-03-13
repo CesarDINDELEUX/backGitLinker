70373501b1a210556d7c59430e77ffca93cb0f61
CesarDINDELEUX <cesar.dindeleux@gmail.com>
2019-03-13 13:22:39 +0100

    Delete tok

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get("/user", (req,res,next) => {
     axios
       .get('https://api.github.com/user', {
         headers: {
-          'Authorization': 'token 16c971dbea6d13b52da8390e6967cfa1cbf76431'
+          'Authorization': ''
         }
       })
       .then((response) => {
@@ -30,7 +30,7 @@ app.get("/orga", (req,res,next) => {
     axios
       .get('https://api.github.com/users/CesarDINDELEUX/orgs', {
         headers: {
-          'Authorization': 'token 16c971dbea6d13b52da8390e6967cfa1cbf76431'
+          'Authorization': ''
         }
       })
       .then((response) => {
@@ -41,12 +41,11 @@ app.get("/orga", (req,res,next) => {
         console.log(err)
       })
 })
-
 function getOrganizations(url) {
     axios
       .get(url, {
         headers: {
-          'Authorization': 'token 16c971dbea6d13b52da8390e6967cfa1cbf76431'
+          'Authorization': ''
         }
       })
       .then((response) => {
